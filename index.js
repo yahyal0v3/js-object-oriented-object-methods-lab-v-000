155162c01b71d48a060a7a1036bff2283176d4f9
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 04:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto = function() { console.log("No, I must disagree") }
 }
